854a548dff581391c0f51b6bc74227d40f6c5679
xg4 <xingor4@gmail.com>
2019-06-30 23:05:15 +0800

    feat: add html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const resolve = dir => path.resolve(__dirname, dir)
 
@@ -6,12 +7,22 @@ module.exports = {
   mode: 'development', // production development
   entry: './src/index.js',
   output: {
-    filename: 'index.min.js',
+    filename: 'index.[hash:6].js',
     path: resolve('dist')
   },
   devServer: {
     port: 3000,
     progress: true,
     compress: true // 压缩
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      filename: 'index.html',
+      minify: {
+        removeAttributeQuotes: true, //删除双引号
+        collapseWhitespace: true // 折叠成一行
+      }
+    })
+  ]
 }
